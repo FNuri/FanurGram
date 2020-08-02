e53a59e8b904a53c2bbd80fecdcbf08b1dc3d82d
FNuri <fowsihashi@hotmail.co.uk>
2020-08-02 23:44:29 +0300

    Removed typo in app title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title><strong>FanurGram</strong></title>
+        <title>FanurGram</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
