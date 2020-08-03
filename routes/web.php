3da61013ee3d71479c9c39c223cc96c4859ded9c
FNuri <fowsihashi@hotmail.co.uk>
2020-08-03 22:19:08 +0300

    Authenticate

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
