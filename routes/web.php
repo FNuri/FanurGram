6079ca4dc211b8595a179f692864777054ca1d7c
FNuri <fowsihashi@hotmail.co.uk>
2020-08-03 22:32:06 +0300

    authenticate

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
